05db91988030386f16aa805ef67b0a5950b941fd
Andrey <a.petrov@itransition.com>
2017-06-15 12:37:52 +0300

    Update tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "ruby_kata"
-require "bubble_sort"
-
-require "minitest/autorun"
-
 require 'simplecov'
 SimpleCov.start
 
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "ruby_kata"
+require "bubble_sort"
+
+require "minitest/autorun"
